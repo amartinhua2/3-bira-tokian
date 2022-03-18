aea1d8611adbc4513cfa825db022da289b54f826
amartinhua2 <93262314+amartinhua2@users.noreply.github.com>
2022-03-18 14:25:49 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 50)
+    basic.pause(3000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 50)
+    basic.pause(3000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    basic.clearScreen()
+})
+DFRobotMaqueenPlus.I2CInit()
